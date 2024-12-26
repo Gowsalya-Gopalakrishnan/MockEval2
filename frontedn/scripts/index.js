089732feb9778007bfecd89fd9da6381f5567e70
Gowsalya-Gopalakrishnan <gowsalyagopalakrishnan1515@gmail.com>
2024-12-26 16:55:18 +0530

    post data

--- frontedn/scripts/index.js
+++ frontedn/scripts/index.js
@@ -4,7 +4,7 @@ form.addEventListener("submit",function(){
     let password = form.password.value;
     // alert("login ");
     if(email === "empher@gmail.com" && password === "empher@123"){
-        alert("Login Success ....");
+        alert("Login Success ....")
         window.location.href="quiz.html";
     }else{
         alert("Login Failed....");
--- frontedn/scripts/quiz.js
+++ frontedn/scripts/quiz.js
@@ -1,2 +1,29 @@
 import { navbar } from "./navbar";
 console.log(navbar);
+postdata();
+function postdata(){
+    let addnew = document.getElementById("addnew");
+    addnew.addEventListener("click",async function(){
+        let questions = document.getElementById("questions");
+        let opt1 =document.getElementById("opt1");
+        let opt2 =document.getElementById("opt2");
+        let opt3 =document.getElementById("opt3");
+        let opt4 =document.getElementById("opt4");
+        let status = document.getElementById("status");
+        let obj = {questions,opt1,opt2,opt3,opt4,status};
+
+        try{
+            let res = await fetch("https://citrine-puzzling-sesame.glitch.me/mock2eva",{
+                method:"POST",
+                headers:{
+                    "content-type":"application/json"
+                },
+                body:JSON.stringify(obj)
+            });
+            // alert("New Question Added...");
+        }
+    
+    })
+}
+
+
